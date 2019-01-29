1e585a6747d7e9de68706550b3e669dfee683e8f
Mateusz Domagala <mateusz@domagala.eu>
2019-01-29 01:48:37 +0100

    poprawka bledu, projekt kompiluje sie i wszystko narazie dziala poprawnie

--- TODOApp/app/src/main/java/zespolowe/todoapp/MainActivity.java
+++ TODOApp/app/src/main/java/zespolowe/todoapp/MainActivity.java
@@ -75,7 +75,7 @@ public class MainActivity extends AppCompatActivity implements TasksRecyclerView
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
+        if (id == R.id.action_workflows)
         {
             goToWorkflowsActivity();
             return true;
